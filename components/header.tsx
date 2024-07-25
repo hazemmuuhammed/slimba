c6b8eec3ab2dd2697562aa6f6ea394957a498a89
hazemmuuhammed <202027220@std.sci.cu.edu.eg>
2024-07-25 13:15:42 +0300

    chore: Refactor CustomDrawerContent component formatting

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
-import useStore from './store';
-import theme from '../hooks/theme';
-import ProgressBar from './ProgressBar';
-import { useNavigation, NavigationProp, ParamListBase } from '@react-navigation/native';
-import { DrawerActions } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import useStore from "./store";
+import theme from "../hooks/theme";
+import ProgressBar from "./ProgressBar";
+import {
+  useNavigation,
+  NavigationProp,
+  ParamListBase,
+} from "@react-navigation/native";
+import { DrawerActions } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Header: React.FC = () => {
   const level = useStore((state) => state.level);
@@ -23,24 +34,27 @@ const Header: React.FC = () => {
     for (let i = 0; i < 7; i++) {
       const date = new Date();
       date.setDate(today.getDate() - i);
-      pastSevenDays.push(date.toISOString().split('T')[0]);
+      pastSevenDays.push(date.toISOString().split("T")[0]);
     }
     setDates(pastSevenDays);
   }, []);
 
   const formatDate = (date: string) => {
-    const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short' };
-    return new Date(date).toLocaleDateString('de-DE', options);
+    const options: Intl.DateTimeFormatOptions = {
+      day: "2-digit",
+      month: "short",
+    };
+    return new Date(date).toLocaleDateString("de-DE", options);
   };
 
-  const handleToggleDay = (direction: 'previous' | 'next') => {
+  const handleToggleDay = (direction: "previous" | "next") => {
     const currentIndex = dates.indexOf(currentDate);
-    if (direction === 'previous' && currentIndex < dates.length - 1) {
+    if (direction === "previous" && currentIndex < dates.length - 1) {
       loadDay(dates[currentIndex + 1]);
-    } else if (direction === 'next' && currentIndex > 0) {
+    } else if (direction === "next" && currentIndex > 0) {
       loadDay(dates[currentIndex - 1]);
     } else {
-      Alert.alert('Info', 'Nur die letzten 7 Tage sind aufrufbar.');
+      Alert.alert("Info", "Nur die letzten 7 Tage sind aufrufbar.");
     }
   };
 
@@ -49,11 +63,11 @@ const Header: React.FC = () => {
   };
 
   const handleLevelPress = () => {
-    navigation.navigate('Level'); // Make sure 'Level' is the correct route name for your Level screen
+    navigation.navigate("Level"); // Make sure 'Level' is the correct route name for your Level screen
   };
 
   const handleInboxPress = () => {
-    navigation.navigate('InboxScreen'); // Make sure 'Inbox' is the correct route name for your Inbox screen
+    navigation.navigate("InboxScreen"); // Make sure 'Inbox' is the correct route name for your Inbox screen
   };
 
   return (
@@ -61,47 +75,73 @@ const Header: React.FC = () => {
       <View style={styles.headerContainer}>
         <View style={styles.leftIcons}>
           <TouchableOpacity onPress={handleMenuPress}>
-            <Ionicons name="menu" size={24} color={theme.colors.primaryGreen100} />
+            <Ionicons
+              name="menu"
+              size={24}
+              color={theme.colors.primaryGreen100}
+            />
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleInboxPress} style={styles.inboxIconContainer}>
-            <Ionicons name="mail-outline" size={24} color={theme.colors.primaryGreen100} />
+          <TouchableOpacity
+            onPress={handleInboxPress}
+            style={styles.inboxIconContainer}
+          >
+            <Ionicons
+              name="mail-outline"
+              size={24}
+              color={theme.colors.primaryGreen100}
+            />
             {unreadMessagesCount > 0 && (
               <View style={styles.unreadBadge}>
-                <Text style={styles.unreadBadgeText}>{unreadMessagesCount}</Text>
+                <Text style={styles.unreadBadgeText}>
+                  {unreadMessagesCount}
+                </Text>
               </View>
             )}
           </TouchableOpacity>
         </View>
         <View style={styles.centerContainer}>
           <TouchableOpacity
-            onPress={() => handleToggleDay('previous')}
+            onPress={() => handleToggleDay("previous")}
             style={styles.backButton}
             disabled={dates.indexOf(currentDate) >= dates.length - 1}
           >
             <Ionicons
               name="arrow-back"
               size={24}
-              color={dates.indexOf(currentDate) >= dates.length - 1 ? theme.colors.primaryGreen20 : theme.colors.primaryGreen100}
+              color={
+                dates.indexOf(currentDate) >= dates.length - 1
+                  ? theme.colors.primaryGreen20
+                  : theme.colors.primaryGreen100
+              }
             />
           </TouchableOpacity>
           <Text style={styles.dateText}>
-            {dates.indexOf(currentDate) === 0 ? `Heute, ${formatDate(currentDate)}` : formatDate(currentDate)}
+            {dates.indexOf(currentDate) === 0
+              ? `Heute, ${formatDate(currentDate)}`
+              : formatDate(currentDate)}
           </Text>
           <TouchableOpacity
-            onPress={() => handleToggleDay('next')}
+            onPress={() => handleToggleDay("next")}
             style={styles.nextButton}
             disabled={dates.indexOf(currentDate) === 0}
           >
             <Ionicons
               name="arrow-forward"
               size={24}
-              color={dates.indexOf(currentDate) === 0 ? theme.colors.primaryGreen20 : theme.colors.primaryGreen100}
+              color={
+                dates.indexOf(currentDate) === 0
+                  ? theme.colors.primaryGreen20
+                  : theme.colors.primaryGreen100
+              }
             />
           </TouchableOpacity>
         </View>
         <TouchableOpacity onPress={handleLevelPress}>
           <View style={styles.starContainer}>
-            <Image source={require('../assets/icons/star.png')} style={styles.starIcon} />
+            <Image
+              source={require("../assets/icons/star.png")}
+              style={styles.starIcon}
+            />
             <Text style={styles.levelText}>{level}</Text>
           </View>
         </TouchableOpacity>
@@ -113,22 +153,24 @@ const Header: React.FC = () => {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     padding: 16,
     paddingBottom: 0,
     paddingTop: 4,
     backgroundColor: theme.colors.white,
   },
   leftIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   centerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    marginRight: 20,
   },
   backButton: {
     marginRight: 8,
@@ -140,48 +182,48 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: theme.fonts.regular,
     color: theme.colors.black,
-    textAlign: 'center',
+    textAlign: "center",
   },
   iconContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   inboxIconContainer: {
     marginLeft: 16,
-    position: 'relative',
+    position: "relative",
   },
   unreadBadge: {
-    position: 'absolute',
+    position: "absolute",
     right: -8,
     top: -8,
-    backgroundColor: 'red',
+    backgroundColor: "red",
     borderRadius: 10,
     paddingHorizontal: 5,
     paddingVertical: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   unreadBadgeText: {
-    color: 'white',
+    color: "white",
     fontSize: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   starContainer: {
-    position: 'relative',
+    position: "relative",
   },
   starIcon: {
     width: 35,
     height: 35,
   },
   levelText: {
-    position: 'absolute',
-    top: '25%', // Adjust this value to move the text down
+    position: "absolute",
+    top: "25%", // Adjust this value to move the text down
     left: 0,
     right: 0,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 14,
     color: theme.colors.black,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontFamily: theme.fonts.bold,
   },
 });
